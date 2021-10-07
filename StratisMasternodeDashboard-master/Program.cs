a7082c735c7fe5dd7360ef7c60a7fc721ca4596c
Francois de la Rouviere <fassadlr@gmail.com>
2021-10-07 14:02:42 +0100

    Fix custom datadir arg

--- StratisMasternodeDashboard-master/Program.cs
+++ StratisMasternodeDashboard-master/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using McMaster.Extensions.CommandLineUtils;
+﻿using McMaster.Extensions.CommandLineUtils;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Stratis.FederatedSidechains.AdminDashboard.Settings;
 
 namespace Stratis.FederatedSidechains.AdminDashboard
 {
@@ -17,13 +13,14 @@ namespace Stratis.FederatedSidechains.AdminDashboard
                 Name = "Stratis.FederatedSidechains.AdminDashboard",
                 Description = "Stratis Federation Members Dashboard"
             };
-            
+
             app.HelpOption(true);
-            
+
             var mainchainportOption = app.Option<int>("--mainchainport <PORT>", "Specify the port that you want to use for the Main Chain", CommandOptionType.SingleValue);
             var sidechainportOption = app.Option<int>("--sidechainport <PORT>", "Specify the port that you want to use for the Side Chain", CommandOptionType.SingleValue);
             var sidechainNodeType = app.Option<string>("--nodetype <NODE>", "Specify the sidechain node type: 10K or 50K", CommandOptionType.SingleValue);
             var environmentType = app.Option<string>("--env <env>", "Specify environment type: testnet or mainnet", CommandOptionType.SingleValue);
+            var dataFolder = app.Option<string>("--datadir <DATADIR>", "(Optional) Specify a custom location for the node's datafolder; This is where the federationKey.dat file will be located.", CommandOptionType.SingleValue);
 
             app.OnExecute(() =>
             {
--- StratisMasternodeDashboard-master/README.md
+++ StratisMasternodeDashboard-master/README.md
@@ -4,8 +4,9 @@ The Stratis Masternode Dashboard provides an overview of the current health stat
 
 ### Usage
 ```
---mainchainport <PORT>  - Specify the port that you want to use for the Main Chain API
---sidechainport <PORT>  - Specify the port that you want to use for the Side Chain
---nodetype <NODE>       - Specify the sidechain node type: 10K or 50K
---env <env> Specify     - Environment type: testnet or mainnet
+--mainchainport <PORT>  		- Specify the port that you want to use for the Main Chain API
+--sidechainport <PORT>  		- Specify the port that you want to use for the Side Chain
+--nodetype <NODE>       		- Specify the sidechain node type: 10K or 50K
+--env <env> Specify     		- Environment type: testnet or mainnet
+--datadir <datadir> Specify     - (Optional) Specify a custom location for the node's datafolder; This is where the federationKey.dat file will be located.
 ```
