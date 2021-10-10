3b2ac51876c846927f6a5767052e07050af093c1
Francois de la Rouviere <fassadlr@gmail.com>
2021-10-10 15:13:50 +0100

    Move producedBlockInLastRound

--- src/StratisMasternodeDashboard/Services/NodeDataService.cs
+++ src/StratisMasternodeDashboard/Services/NodeDataService.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.Extensions.Logging;
-using NBitcoin;
-using NBitcoin.DataEncoders;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Stratis.FederatedSidechains.AdminDashboard.Entities;
-using Stratis.FederatedSidechains.AdminDashboard.Settings;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
+using NBitcoin.DataEncoders;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Stratis.FederatedSidechains.AdminDashboard.Entities;
+using Stratis.FederatedSidechains.AdminDashboard.Settings;
 namespace Stratis.FederatedSidechains.AdminDashboard.Services
 {
     public abstract class NodeDataService
@@ -239,7 +239,7 @@ namespace Stratis.FederatedSidechains.AdminDashboard.Services
                 var response = await apiRequester.GetRequestAsync(endpoint, "/api/Federation/members/current").ConfigureAwait(false);
                 sidechainMinerStats.BlockProducerHits = response.Content.miningStats.minerHits;
                 sidechainMinerStats.BlockProducerHitsValue = (int)(response.Content.miningStats.minerHits / (float)response.Content.federationSize * 100);
-                sidechainMinerStats.ProducedBlockInLastRound = (bool)response.Content.producedBlockInLastRound;
+                sidechainMinerStats.ProducedBlockInLastRound = (bool)response.Content.miningStats.producedBlockInLastRound;
             }
             catch (Exception ex)
             {
