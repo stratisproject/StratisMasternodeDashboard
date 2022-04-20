bde729ee9adfe2fee185ed6f0046f350a8122ec9
Satya Karki <spacegrlsatya@gmail.com>
2022-04-20 13:32:52 +0600

    Add Block hash link. Formatted Unconfirmed amount. Display wallet address and total peers using Signalr.

--- src/StratisMasternodeDashboard/wwwroot/js/nodeevent.js
+++ src/StratisMasternodeDashboard/wwwroot/js/nodeevent.js
@@ -15,18 +15,28 @@ connection.on("receiveEvent", function (message) {
     if (message.height) {
         document.getElementById('lblSidechainNodeBlockHeight').innerHTML = ` ${message.height}`;
     }
-
+    
     if (message.hash) {
         document.getElementById('lblSidechainNodeHash').innerHTML = ` ${message.hash}`;
+        var hashelement = document.getElementById("sidechainBlockHash");
+        hashelement.setAttribute('href', "https://chainz.cryptoid.info/cirrus/block.dws?" + ` ${message.hash}` + ".htm");
+    }   
+    if (message.connectedNodes) {
+        document.getElementById('lblSidechainTotalConnectedNode').innerHTML = ` ${message.connectedNodes}` + ' /';
     }
-   
     if (message.accountsBalances) {
         var confirmedAmount = (((` ${message.accountsBalances[0].amountConfirmed}`) / 100000000).toFixed(8));
         var parts = confirmedAmount.toString().split(".");
         var confirmedAmountwithComma = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
         document.getElementById('lblSidechainAmountConfirmed').innerHTML = confirmedAmountwithComma;
 
-        document.getElementById('lblSidechainAmountUnConfirmed').innerHTML = ` ${message.accountsBalances[0].amountUnconfirmed}`;
+        var unconfirmedAmount = (((` ${message.accountsBalances[0].amountUnconfirmed}`) / 100000000).toFixed(8));
+        var unconfirmedparts = unconfirmedAmount.toString().split(".");
+        var unConfirmedAmountwithComma = unconfirmedparts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (unconfirmedparts[1] ? "." + unconfirmedparts[1] : "");
+        document.getElementById('lblSidechainAmountUnConfirmed').innerHTML = unConfirmedAmountwithComma;
     }
-
+    if (message.accountsBalances) {
+        document.getElementById('sidechainMiningAddress').innerHTML = ` ${message.accountsBalances[0].addresses[0].address}`;        
+        document.getElementById('lblSidechainMiningWalletAddress').innerHTML = ` ${message.accountsBalances[0].addresses[0].address}`;
+    }
 });
